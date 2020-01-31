2fef1a3b156ea6184b728f05fb3fbf6c2ad67c7d
Liam <liamt@au1.ibm.com>
2020-01-31 23:33:07 +1100

    EventEmitters work.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EventEmitter3 from 'eventemitter3'
+
+function Adder(props) { 
+  const textfield = useRef(null);
+  
+  return (<div>
+    <input ref={textfield}/>
+    <button onClick={()=>{
+      props.emitter.emit('add_todo', textfield.current.value)
+    }}>Add Todo</button> 
+  </div>);
+}
+
+function List(props) { 
+
+  let todos = props.todos.map( (todo, i) => <div key={i}>{todo}</div>)
+
+  return (<div>
+    {todos}
+  </div>);
+}
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [eventEmitter, setEventEmitter] = useState(
+    new EventEmitter3() 
+  );
+
+  useEffect( () => {
+    eventEmitter.on("add_todo", (todo)=>{
+      const newTodoObject = [...todos, todo]; 
+      setTodos(newTodoObject)
+    }, {})
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO APPLICATION</h1>
+       <Adder emitter={eventEmitter}> </Adder>
+       <List todos={todos}> </List>
     </div>
   );
 }
